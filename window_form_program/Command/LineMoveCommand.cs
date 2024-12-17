e8689f0630a2b6892e52c8ceceefb4d6d062dfa8
alan <t111590053@ntut.org.tw>
2024-12-17 14:55:50 +0800

    line move command

---
+++ window_form_program/Command/LineMoveCommand.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2
+{
+    public class LineMoveCommand : ICommand
+    {
+        Shape shape;
+        Model model;
+        Line line;
+        PointF startPoint;
+        PointF endPoint ;
+        Line startLine = new Line();
+        Line endLine =new Line();
+        public LineMoveCommand(Model m, Shape shape, PointF startPoint, PointF endPoint, Line movedLine,Line startLine,Line endLine)
+        {
+            this.shape = shape;
+            this.line = movedLine;
+            model = m;
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+
+            this.startLine.X = startLine.X;
+            this.startLine.Y = startLine.Y;
+            this.startLine.Width = startLine.Width;
+            this.startLine.Height = startLine.Height;
+
+            this.endLine.X = endLine.X;
+            this.endLine.Y = endLine.Y;
+            this.endLine.Width = endLine.Width;
+            this.endLine.Height = endLine.Height;
+        }
+
+        public void Execute()
+        {
+            shape.OrangeDot = new PointF(shape.OrangeDot.X + endPoint.X - shape.X, shape.OrangeDot.Y + endPoint.Y - shape.Y);
+            shape.X = endPoint.X;
+            shape.Y = endPoint.Y;
+            line.X = endLine.X;
+            line.Y = endLine.Y;
+            line.Width = endLine.Width;
+            line.Height = endLine.Height;
+        }
+
+        public void UnExecute()
+        {
+            line.X = startLine.X;
+            line.Y = startLine.Y;
+            line.Width = startLine.Width;
+            line.Height = startLine.Height;
+            shape.OrangeDot = new PointF(shape.OrangeDot.X + startPoint.X - shape.X, shape.OrangeDot.Y + startPoint.Y - shape.Y);
+            shape.X = startPoint.X;
+            shape.Y = startPoint.Y;
+            
+
+        }
+    }
+}
--- window_form_program/State/GeneralState.cs
+++ window_form_program/State/GeneralState.cs
@@ -14,16 +14,21 @@ namespace hw2
         List<Shape> selectedShapes = new List<Shape>();
         const int CTRL_KEY = 17;
         bool isCtrlKeyDown;
+        bool isMovingLine;
         bool isMouseDown;
         bool isMovingText;
         PointF mouseDownPosition = new PointF();
         PointF startPoint = new PointF();
         PointF endPoint = new PointF();
+        Line startLine = new Line();
+        Line endLine = new Line();
+        Line movedLine;
         public void Initialize(Model m)
         {
             selectedShapes.Clear();
             isCtrlKeyDown = false;
             isMovingText = false;
+            isMovingLine = false;
         }
         public List<Shape> GetSelectShapes()
         {
@@ -80,26 +85,46 @@ namespace hw2
                     shape.X += point.X - mouseDownPosition.X;
                     shape.Y += point.Y - mouseDownPosition.Y;
                     shape.OrangeDot = new PointF(shape.OrangeDot.X + (point.X - mouseDownPosition.X), shape.OrangeDot.Y + (point.Y - mouseDownPosition.Y));
+                    endPoint = new PointF(shape.X, shape.Y);
+
                     foreach (Shape line in m.GetShapes())
                     {
                         if (line.IsLine())
                         {
                             Line LineID = (Line)line;
-                            if (LineID.HeadShapeID == shape.ID)
+                            if(LineID.HeadShapeID == shape.ID || LineID.TailShapeID == shape.ID)
                             {
-                                line.X += point.X - mouseDownPosition.X;
-                                line.Y += point.Y - mouseDownPosition.Y;
-                                line.Width -= point.X - mouseDownPosition.X;
-                                line.Height -= point.Y - mouseDownPosition.Y;
+                                if (!isMovingLine)
+                                {
+                                    startLine.X = LineID.X;
+                                    startLine.Y = LineID.Y;
+                                    startLine.Width = LineID.Width;
+                                    startLine.Height = LineID.Height;
+                                    movedLine = LineID;
+                                    isMovingLine = true;
+                                }
+
+                                if (LineID.HeadShapeID == shape.ID)
+                                {
+                                    line.X += point.X - mouseDownPosition.X;
+                                    line.Y += point.Y - mouseDownPosition.Y;
+                                    line.Width -= point.X - mouseDownPosition.X;
+                                    line.Height -= point.Y - mouseDownPosition.Y;
+
+                                }
+                                else 
+                                {
+                                    line.Width += point.X - mouseDownPosition.X;
+                                    line.Height += point.Y - mouseDownPosition.Y;
+                                }
+                                endLine = LineID;
+                                break;
                             }
-                            if (LineID.TailShapeID == shape.ID)
-                            {
-                                line.Width += point.X - mouseDownPosition.X;
-                                line.Height += point.Y - mouseDownPosition.Y;
-                            }
+                            
+                            
                         }
                     }
-                    endPoint = new PointF(shape.X, shape.Y);
+                    
                 }
                 mouseDownPosition = point;
             }
@@ -116,14 +141,29 @@ namespace hw2
                 isMouseDown = false;
                 isMovingText = false;
             }
-            if (isMouseDown && selectedShapes.Count > 0)
+            else if (isMovingLine)
             {
-                if(startPoint!=endPoint)
-                    m.commandManager.Execute(new MoveCommand(m, selectedShapes[0],startPoint,endPoint));
+                if (startPoint != endPoint)
+                {
+                    m.commandManager.Execute(new LineMoveCommand(m, selectedShapes[0], startPoint, endPoint,movedLine,startLine,endLine));
+                }
                 mouseDownPosition.X = -1;
                 mouseDownPosition.Y = -1;
                 isMouseDown = false;
                 isMovingText = false;
+                isMovingLine = false;
+            }
+            else if (isMouseDown && selectedShapes.Count > 0)
+            {
+                if (startPoint != endPoint)
+                {
+                    m.commandManager.Execute(new MoveCommand(m, selectedShapes[0], startPoint, endPoint));
+                }
+                mouseDownPosition.X = -1;
+                mouseDownPosition.Y = -1;
+                isMouseDown = false;
+                isMovingText = false;
+                isMovingLine = false;
             }
         }
 
